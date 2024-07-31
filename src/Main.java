ebd88ce28159081bc904f8236d47ccf05bd09a1d
Marisa-T <marisa.tivadar@yahoo.com>
2024-07-31 15:11:41 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Marisa");
+
+        System.out.println(4+5);
+        System.out.println(12/2);
+        System.out.println(13/4);
+
+        int a = 13;
+        int b = 4;
+        float result = (float)a/b;
+        System.out.println(result);
+
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + -3 * 5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+        }
+    }
